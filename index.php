f0114836eeffc51ee8ca3d437a2f60c37c829d3b
PondJaTH <palapon2545@gmail.com>
2019-12-10 15:04:44 +0700

    update

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    if (isset($_GET['type'])) {
+        if ($_GET['type'] == 'engi') {
+            header("Location: ./engi.html");
+        } else if ($_GET['type'] == 'pharm') {
+            header("Location: ./pharm.html");
+        }
+    } else {
+        echo 'Hello World';
+    }
